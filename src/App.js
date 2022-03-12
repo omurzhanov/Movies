6968a19feeba6f7c67e6535e6360d22b7b466193
Zalkar <omurzhanovz@gmail.com>
2022-03-12 01:19:29 +0100

    first front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddMovie from './AddMovie';
 function App() {
+   
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AddMovie/>
     </div>
   );
 }
-
 export default App;
